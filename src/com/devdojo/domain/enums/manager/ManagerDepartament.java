eb9359cabd2d268e7f09736f4d73933e9cbc9bb1
PHnaves <pnaves001@gmail.com>
2025-09-21 18:45:23 -0300

    feat: create enums ManagerDepartament and ManagerResponsibility for Manager class

---
+++ src/com/devdojo/domain/enums/manager/ManagerDepartament.java
@@ -0,0 +1,10 @@
+package com.devdojo.domain.enums.manager;
+
+public enum ManagerDepartament {
+    HR,         // Recursos Humanos
+    IT,         // Tecnologia da Informação
+    FINANCE,    // Finanças
+    SALES,      // Vendas
+    MARKETING,  // Marketing
+    OPERATIONS  // Operações
+}
---
+++ src/com/devdojo/domain/enums/manager/ManagerResponsibility.java
@@ -0,0 +1,9 @@
+package com.devdojo.domain.enums.manager;
+
+public enum ManagerResponsibility {
+    TEAM_MANAGEMENT,      // Gestão de equipe
+    BUDGET_APPROVAL,      // Aprovação de orçamento
+    STRATEGIC_PLANNING,   // Planejamento estratégico
+    PERFORMANCE_REVIEW,   // Avaliação de desempenho
+    PROJECT_SUPERVISION   // Supervisão de projetos
+}
