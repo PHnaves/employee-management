28d1be2a686bff9616a997c29fdfab5d767398c3
PHnaves <pnaves001@gmail.com>
2025-09-24 21:25:46 -0300

    test: add basic tests for DeveloperServiceImpl methods

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
+import com.devdojo.domain.Developer;
+import com.devdojo.domain.Employee;
+import com.devdojo.domain.Person;
+import com.devdojo.domain.Project;
+import com.devdojo.domain.enums.Developer.DeveloperExperience;
+import com.devdojo.domain.enums.Developer.DeveloperLanguages;
+import com.devdojo.domain.enums.Developer.DeveloperSpecialty;
+import com.devdojo.domain.enums.EmployeeBenefits;
+import com.devdojo.service.impl.DeveloperServiceImpl;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        EmployeeBenefits[] employeeBenefits = new EmployeeBenefits[]{EmployeeBenefits.DENTAL_PLAN, EmployeeBenefits.FOOD_VOUCHER};
+        DeveloperLanguages[] developerLanguages = new DeveloperLanguages[]{DeveloperLanguages.CSHARP, DeveloperLanguages.JAVA};
+        Employee developer = new Developer(12, "EU", "2334455", 34, Person.Sex.MASCULINO, "pmabesiji@gmail", "234567777", 1235465.89, employeeBenefits, DeveloperSpecialty.BACKEND, developerLanguages, DeveloperExperience.PLENO);
+
+        Developer[] developers = new Developer[]{(Developer) developer};
+        Project project = new Project("Titulo 1", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", developers, "ontem", "hoje");
+
+        ((Developer) developer).setProject(project);
+
+        DeveloperServiceImpl developerService = new DeveloperServiceImpl();
+
+        developerService.technicalInformations((Developer) developer);
+        developerService.project((Developer) developer);
     }
 }
