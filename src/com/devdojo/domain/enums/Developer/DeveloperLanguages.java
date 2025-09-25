fd4578a02fb5360df6e6c23768e7c8735e5a0b98
PHnaves <pnaves001@gmail.com>
2025-09-24 21:23:13 -0300

    refactor: add values to DeveloperLanguages enum
    feat: extract scope from methods technicalInformations and project in DeveloperServiceImpl

--- src/com/devdojo/domain/enums/Developer/DeveloperLanguages.java
+++ src/com/devdojo/domain/enums/Developer/DeveloperLanguages.java
@@ -1,12 +1,22 @@
 package com.devdojo.domain.enums.Developer;
 
 public enum DeveloperLanguages {
-    JAVA,
-    PHP,
-    JAVA_SCRIPT,
-    COBOL,
-    GOLANG,
-    CSHARP,
-    TYPE_SCRIPT,
-    SWIFT
+    JAVA("Java, puro suco do milho tang"),
+    PHP("PHP monstro"),
+    JAVA_SCRIPT("JavaScript pepa"),
+    COBOL("COBOL"),
+    GOLANG("GOLANG"),
+    CSHARP("C#"),
+    TYPE_SCRIPT("TypeScript pepa deluxe"),
+    SWIFT("Swift");
+
+    private String languages;
+
+    DeveloperLanguages(String languages ) {
+        this.languages = languages;
+    }
+
+    public String getLanguages() {
+        return languages;
+    }
 }
--- src/com/devdojo/service/impl/DeveloperServiceImpl.java
+++ src/com/devdojo/service/impl/DeveloperServiceImpl.java
@@ -2,6 +2,8 @@ package com.devdojo.service.impl;
 
 import com.devdojo.domain.Developer;
 import com.devdojo.domain.Employee;
+import com.devdojo.domain.Project;
+import com.devdojo.domain.enums.Developer.DeveloperLanguages;
 import com.devdojo.service.DeveloperService;
 import com.devdojo.service.EmployeeService;
 
@@ -10,11 +12,29 @@ public class DeveloperServiceImpl implements DeveloperService {
 
     @Override
     public void technicalInformations(Developer developer) {
-
+        System.out.println("Informacoes tecnicas");
+        System.out.println("Nivel de experiencia: " + developer.getDeveloperExperience());
+        System.out.println("Especialidade: " + developer.getSDeveloperSpecialty());
+        System.out.println("Linguagens utilizadas");
+        for (DeveloperLanguages developerLanguages : developer.getDeveloperLanguages()) {
+            System.out.println(developerLanguages.getLanguages());
+        }
     }
 
     @Override
     public void project(Developer developer) {
+        System.out.println("Projeto atual");
+        System.out.println("Titulo do projeto" + developer.getProject().getTitle());
+        System.out.println("Descricao");
+        System.out.println(developer.getProject().getDescription());
+        if (developer.getProject().getDevelopers() != null ) {
+            System.out.println("Equipe");
+            for (Developer developer1 : developer.getProject().getDevelopers()) {
+                System.out.println(developer1.getName());
+            }
+        }else {
+            System.out.println("Projeto sem membros no momento");
+        }
 
     }
 
